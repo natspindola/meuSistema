1619ded138dca69f38f2aabf48ac51761dfff35a
natspindola <natspindola@hotmail.com>
2022-04-19 17:59:40 -0300

    inicio projeto

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ namespace meuSistema
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
